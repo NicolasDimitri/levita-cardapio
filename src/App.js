91597b5a0915bcaf10abcf6dfe8658a41a998f47
NicolasDimitri <nicolasdodimitri@gmail.com>
2025-08-19 17:03:18 -0300

    criação do OrderDetails

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import './App.css';
 import { Categorie } from './components/categorie';
 import { Items } from './components/Itens';
 import { Header } from './components/header';
+import { OrderDetails } from './components/OrderDetails';
 
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Categorie />
-      <Items />
-      <Categorie />
-      <Items />
-      <Categorie />
-      <Items />
-      <Categorie />
-      <Items />
-    </div>
+    // <div className="App">
+    //   <Header />
+    //   <Categorie />
+    //   <Items />
+    //   <Categorie />
+    //   <Items />
+    //   <Categorie />
+    //   <Items />
+    //   <Categorie />
+    //   <Items />
+    // </div>
+    <OrderDetails />
   );
 }
 
---
+++ src/components/OrderDetails.js
@@ -0,0 +1,56 @@
+export const OrderDetails = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        border: '1px solid black',
+        borderRadius: '10px',
+        width: '900px',
+        height: '800px',
+      }}
+    >
+      <div
+        style={{
+          border: '1px solid black',
+          borderRadius: '10px',
+          width: '800px',
+          height: '700px',
+        }}
+      >
+        <div>
+          <div
+            style={{
+              border: '1px solid black',
+              borderRadius: '10px',
+              display: 'flex',
+              justifyContent: 'flex-end',
+              backgroundColor: 'red',
+            }}
+          >
+            <div
+              style={{
+                border: '1px solid black',
+                borderRadius: '10px',
+                width: '40px',
+                height: '40px',
+                // backgroundColor: 'red'
+              }}
+            ></div>
+          </div>
+          <div
+            style={{
+              border: '1px solid black',
+              borderRadius: '10px',
+              width: '404px',
+              height: '404px',
+              // backgroundColor: 'red'
+            }}
+          >
+            <h1>fxgsgs do Pedido</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
